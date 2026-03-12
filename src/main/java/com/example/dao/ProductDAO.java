6f5cb5efe107f9b1ba2d3e5d74e3e749aaba4f98
Gopalpawar <kp015770gmail.com>
2026-03-12 19:10:03 +0530

    Product DAO class created and list and entity manager

--- src/main/java/com/example/dao/ProductDAO.java
+++ src/main/java/com/example/dao/ProductDAO.java
@@ -1,4 +1,32 @@
 package com.example.dao;
 
+import com.example.entity.Product;
+import com.example.util.JPAUtil;
+
+import jakarta.persistence.EntityManager;
+import java.util.List;
+
 public class ProductDAO {
+
+    public void saveProduct(Product product) {
+
+        EntityManager entityManager = JPAUtil.getEntityManager();
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(product);
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+    }
+
+    public List<Product> getAllProducts() {
+
+        EntityManager entityManager = JPAUtil.getEntityManager();
+
+        List<Product> list =
+                entityManager.createQuery("from Product", Product.class).getResultList();
+
+        entityManager.close();
+        return list;
+    }
 }
