f8074054dfada1df8ce398e406b44d2c108fc0bf
Gopalpawar <kp015770gmail.com>
2026-03-12 19:14:59 +0530

    Order Item DAO class created for saveing order item

--- src/main/java/com/example/dao/OrderItemDAO.java
+++ src/main/java/com/example/dao/OrderItemDAO.java
@@ -1,4 +1,20 @@
 package com.example.dao;
 
+import com.example.entity.OrderItem;
+import com.example.util.JPAUtil;
+
+import jakarta.persistence.EntityManager;
+
 public class OrderItemDAO {
+
+    public void saveOrderItem(OrderItem item) {
+
+        EntityManager entityManager = JPAUtil.getEntityManager();
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(item);
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+    }
 }
