4d816008eca1fe3d2173d54ac8ad8475dc7eea36
Gopalpawar <kp015770gmail.com>
2026-03-12 19:19:43 +0530

    order entity created with entity class and generated values.

--- src/main/java/com/example/entity/Order.java
+++ src/main/java/com/example/entity/Order.java
@@ -1,4 +1,54 @@
 package com.example.entity;
 
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.*;
+
+@Entity
+@Table(name = "orders")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int orderId;
+
+    private String customerName;
+
+    private LocalDate orderDate;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> items = new ArrayList<>();
+
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
 }
