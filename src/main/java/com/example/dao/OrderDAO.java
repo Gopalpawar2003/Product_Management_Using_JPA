b4b3846d13da3f64bde9fb993bb7dae209f658cd
Gopalpawar <kp015770gmail.com>
2026-03-12 19:12:06 +0530

    Order DAO class created and list and enitity manager added

--- src/main/java/com/example/dao/OrderDAO.java
+++ src/main/java/com/example/dao/OrderDAO.java
@@ -1,4 +1,32 @@
 package com.example.dao;
 
+import com.example.entity.Order;
+import com.example.util.JPAUtil;
+
+import jakarta.persistence.EntityManager;
+import java.util.List;
+
 public class OrderDAO {
+
+    public void saveOrder(Order order) {
+
+        EntityManager entityManager = JPAUtil.getEntityManager();
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(order);
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+    }
+
+    public List<Order> getAllOrders() {
+
+        EntityManager entityManager = JPAUtil.getEntityManager();
+
+        List<Order> orders =
+                entityManager.createQuery("from Order", Order.class).getResultList();
+
+        entityManager.close();
+        return orders;
+    }
 }
